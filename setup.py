f2880ff61fdd685ef67ea8f16070e13000ab7ca2
Anthony Keelan <anthony.keelan@gmail.com>
2018-11-27 10:16:38 -0700

    flake8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='MC_Random',
     version='0.1dev',
-    packages=['mc_random',],
+    packages=['mc_random', ],
     license='MIT license',
     long_description=open('README.md').read(),
 )
